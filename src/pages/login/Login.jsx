942bbd03f841ee50935e9e1100616f23920f6659
Ayeza <ayezabashir442@gmail.com>
2023-07-26 00:36:06 +0500

    login component

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,28 +1,27 @@
 import React from 'react'
-import loginimg from '../../assets/images/loginimg.png'
+import loginimg from '../../assets/images/accountbg.jpg'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
         <>
             <section className="login">
-                <div className="container">
-                    <h3>My Account Information</h3>
-                    <div className="content">
-                        <div className="left">
-                            <img src={loginimg} alt="images" />
-                        </div>
-                        <div className="right">
-                            <form>
-                                <label htmlFor="username">Username</label>
-                                <input type="text" id='username' />
-                                <label htmlFor="email">Email</label>
-                                <input type="text" id='email' />
-                                <label htmlFor="password">Password</label>
-                                <input type="text" id='password' />
-                                <button className="btn">Update</button>
-                            </form>
+                <div className="content">
+                    <div className="bgimg">
+                        <img src={loginimg} alt="images" />
+                        <div className="text">
+                            <h3>Login</h3>
+                            <h1>My Account</h1>
                         </div>
                     </div>
+                    <form>
+                        <label htmlFor="username">Username or Email address</label>
+                        <input type="text" id='username' required />
+                        <label htmlFor="password">Password</label>
+                        <input type="text" id='password' required />
+                        <button className='btn'>Login</button>
+                        <Link to="/register">Register</Link>
+                    </form>
                 </div>
             </section>
         </>
